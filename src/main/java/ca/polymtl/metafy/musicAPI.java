2b10e50d8fb91700cde56f6a423fb3adf9079e39
LeRongeur <ulric.villeneuve@polymtl.ca>
2018-02-28 15:00:42 -0500

    Premiere ebauche de requete chez Jamendo. Pour l'instant, on est capable de faire une recherche!

--- src/main/java/ca/polymtl/metafy/musicAPI.java
+++ src/main/java/ca/polymtl/metafy/musicAPI.java
@@ -10,26 +10,27 @@ public class musicAPI {
     public static void main(String[] args) throws Exception {
         Client client = ClientBuilder.newClient();
 
-        WebTarget resource = client.target("https://accounts.spotify.com/api/token");
+        /*WebTarget resource = client.target("http://api.jamendo.com/v3.0");
 
         Form form = new Form();
         form.param("grant_type", "client_credentials");
 
         Invocation.Builder request = resource.request();
         request.accept(MediaType.APPLICATION_JSON);
-        request.header("Authorization", "Basic " + ApiKeyLoader.getInstance().getApiKey("Spotify"));
-        AuthorizationReturnDTO response = request.post(Entity.form(form), AuthorizationReturnDTO.class);
+        request.header("Authorization", "Basic " + ApiKeyLoader.getInstance().getApiKey("Jamendo"));
+        AuthorizationReturnDTO response = request.post(Entity.form(form), AuthorizationReturnDTO.class);*/
 
-        System.out.println(response);
+        //System.out.println(response);
 
-        resource = client.target("https://api.spotify.com/v1/search?q=adele&type=track&limit=10");
+        WebTarget resource = client.target("https://api.jamendo.com/v3.0/tracks/?client_id=dacd3181&format=jsonpretty&limit=2&fuzzytags=groove+rock&speed=high+veryhigh&include=musicinfo&groupby=artist_id");
 
         Invocation.Builder request2 = resource.request();
         request2.accept(MediaType.APPLICATION_JSON);
-        request2.header("Authorization", "Bearer " + response.getAccessToken());
-        SearchReturnDTO response2 = request2.get(SearchReturnDTO.class);
+        //request2.header("Authorization", "Bearer " + response.getAccessToken());
+        //SearchReturnDTO response2 = request2.get(SearchReturnDTO.class);
+        System.out.println(request2.get().readEntity(String.class));
 
-        System.out.println(response2);
+        //System.out.println(response2);
         /*if (response.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
             System.out.println("Success! " + response.getStatus());
             System.out.println(response.getEntity().toString());
