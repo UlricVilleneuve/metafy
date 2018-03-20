fc8208a10c1610f218ce78206c538aaed422a50d
William MOUCHERE <william.mouchere@insa-rennes.fr>
2018-03-20 14:58:38 -0400

    Add playlist playing

--- src/main/webapp/js/playlists.js
+++ src/main/webapp/js/playlists.js
@@ -1,11 +1,34 @@
 "use strict"
 let server = "http://localhost:8080/api/";
 
+var playlistTracks;
+var currentPlaylist = -1;
+var playlistIndex = 0;
+
+var audio = document.getElementById("audioplayer");
+
 document.getElementById('query').onkeydown = function(e){
     if(e.keyCode == 13){
         submit()
     }
 };
+
+audio.addEventListener('ended',function(){
+    if(currentPlaylist != -1) {
+        if(playlistIndex < playlistTracks[currentPlaylist].length) {
+            let info = `<source src=${playlistTracks[currentPlaylist][playlistIndex]} type="audio/mpeg">`
+            audio.innerHTML = info
+            playlistIndex++;
+            audio.load();
+            audio.play();
+        } else {
+            audio.innerHTML = "";
+            audio.load();
+            currentPlaylist = -1;
+            playlistIndex = 0;
+        }
+    }
+});
 
 function submit(){
     let query = document.getElementById('query').value;
@@ -28,6 +51,8 @@ function submit(){
 function findAllPlaylists() {
     let req = new XMLHttpRequest();
 
+    playlistTracks = {};
+
     req.onreadystatechange = function () {
         if (req.readyState !== 4) return;
 
@@ -37,10 +62,17 @@ function findAllPlaylists() {
             let playlists = JSON.parse(req.response);
             console.log(playlists);
 
+            let urls = [];
+
             let info = "";
             playlists.forEach(function (p) {
+                p.tracks.forEach(function (t) {
+                    urls.push(t.url);
+                });
+                playlistTracks[p.id] = urls;
                 info += `<div name="playlist">`
                 info += `<label class="playlist-title">${p.name}\t</label>`
+                info += `<button type="submit" onclick="playTracks(${p.id})" class="pure-button"><i class="fas fa-play"></i></button>`
                 info += `<button type="submit" value="Supprimer" class="pure-button button-delete"><i class="fas fa-trash"></i></button>`
                 info += `<input name="data" type="hidden" value=${encodeURIComponent(JSON.stringify(p))} />`
                 info += `<ol>`
@@ -53,7 +85,7 @@ function findAllPlaylists() {
 
                     //Display play button only if link for preview is available
                     if(t.url != undefined){
-                        info += `<a href=${t.url} target="_blank" class="pure-button"><i class="fas fa-play"></i></a>`
+                        info += `<button type="submit" onclick="playTrack('${t.url}')" class="pure-button"><i class="fas fa-play"></i></button>`
                     }
                     info += `</li>`;
                 })
@@ -70,7 +102,7 @@ function findAllPlaylists() {
         for(let i = 0; i < playlists.length; i++) {
             let data = JSON.parse(decodeURIComponent(playlists[i].getElementsByTagName('input')[0].getAttribute("value")));
             console.log(data);
-            playlists[i].getElementsByTagName('button')[0].onclick = function () {
+            playlists[i].getElementsByTagName('button')[1].onclick = function () {
                 let req2 = new XMLHttpRequest();
                 req2.onreadystatechange = update();
                 req2.open("DELETE", server + "playlist/" + data.id, true);
@@ -87,5 +119,26 @@ var update = function () {
         findAllPlaylists();
     }
 };
+
+function playTrack(url) {
+    let info = `<source src=${url} type="audio/mpeg">`
+    audio.innerHTML = info
+    playlistIndex = 0;
+    currentPlaylist = -1;
+    audio.pause()
+    audio.load()
+    audio.play()
+}
+
+function playTracks(id) {
+    let urls = playlistTracks[id]
+    let info = `<source src=${urls[0]} type="audio/mpeg">`
+    currentPlaylist = id;
+    playlistIndex = 1;
+    audio.innerHTML = info
+    audio.pause()
+    audio.load()
+    audio.play()
+}
 
 findAllPlaylists();
